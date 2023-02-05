9ac3d65cae8210d6cd0a9b558be51af07275c8ec
Prakash Kafle <kafleprakash96@gmail.com>
2023-02-05 02:36:07 -0600

    Updated service to call new url on spring boot

--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -15,10 +15,12 @@ export class ProductService {
 
   getProductList(theCategoryId:number): Observable<Product[]> {
 
-    //Build url based on given category id
+    // Build url based on given category id
+
+    const searchUrl = `${this.baseUrl}/search/findByCategoryId?id=${theCategoryId}`;
 
 
-    return this.httpClient.get<GetResponse>(this.baseUrl).pipe(
+    return this.httpClient.get<GetResponse>(searchUrl).pipe(
       map(response => response._embedded.products)
     );
   }
