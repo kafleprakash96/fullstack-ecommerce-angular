c86030b3242560eed3d15bad93b2d79572fbbfe2
Prakash Kafle <kafleprakash96@gmail.com>
2023-02-11 20:01:08 -0600

    Updated cart status on product details

--- src/app/components/product-details/product-details.component.ts
+++ src/app/components/product-details/product-details.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { CartItem } from 'src/app/common/cart-item';
 import { Product } from 'src/app/common/product';
+import { CartService } from 'src/app/services/cart.service';
 import { ProductService } from 'src/app/services/product.service';
 
 @Component({
@@ -13,7 +15,8 @@ export class ProductDetailsComponent {
   product! : Product;
 
   constructor(private productService : ProductService,
-              private route: ActivatedRoute){
+              private route: ActivatedRoute,
+              private cartService: CartService){
 
   }
 
@@ -33,6 +36,13 @@ export class ProductDetailsComponent {
         this.product = data;
       }
     )
+  }
+
+  addToCart(){
+
+    const theCartItem = new CartItem(this.product);
+    this.cartService.addToCart(theCartItem);
+
   }
 
 }
