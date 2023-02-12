0bebc01add419bcd013b21c50eea08a4fa90be15
Prakash Kafle <kafleprakash96@gmail.com>
2023-02-12 01:13:53 -0600

    Updated cart details with price and quantity

--- src/app/components/cart-details/cart-details.component.ts
+++ src/app/components/cart-details/cart-details.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { CartItem } from 'src/app/common/cart-item';
+import { CartService } from 'src/app/services/cart.service';
 
 @Component({
   selector: 'app-cart-details',
@@ -6,5 +8,34 @@ import { Component } from '@angular/core';
   styleUrls: ['./cart-details.component.css']
 })
 export class CartDetailsComponent {
+
+  cartItems : CartItem[] = [];
+
+  totalPrice : number = 0;
+  totalQuantity : number = 0;
+
+  constructor(private cartService:CartService){
+
+  }
+
+  ngOnInit() : void {
+    this.listCartDetails();
+  }
+  listCartDetails() {
+      // get handle to the cart items
+      this.cartItems = this.cartService.cartItems;
+
+      //subscribe to totalPrice and totalQuantity
+      this.cartService.totalPrice.subscribe(
+        data => this.totalPrice = data
+      )
+
+      this.cartService.totalQuantity.subscribe(
+        data => this.totalQuantity = data
+      )
+
+      //compute totalprice and quantity
+      this.cartService.computeCartTotals();
+  }
 
 }
